fe6ff2f6a9d02a82117a4924e07d811c0a93b739
imoh11 <ghamjd89@gmail.com>
2025-06-18 11:11:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const COMPLAINTS_WEBHOOK_URL = 'https://hook.us2.make.com/4ij8kumta41ozellaauixdc25zm7i27k';
-const GOOGLE_SHEETS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbz6T1Q-joOQrH3i4y7LP5UyuQN20aSryQmnjEOfA7BqPsGtuH-FaKUVcsUwjVYTb0KCcA/exec'; // استبدل هذا بالرابط الخاص بك!
+const GOOGLE_SHEETS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbybMDAtCZG5SvIzmP76Ob54DzOOyaP_2Om1g5LBpmrZy8B9H9PYUTNWNxSOYTgHMjWs/exec'; // استبدل هذا بالرابط الخاص بك!
 
 document.addEventListener('DOMContentLoaded', function() {
     const fixedNavBar = document.querySelector('.fixed-nav-bar');
